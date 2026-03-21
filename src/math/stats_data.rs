ce7bce27b8624dd5e3a13bb87f5505338ca44a94
ttk-adm <218129711+ttk-adm@users.noreply.github.com>
2026-03-21 10:48:54 +0100

    add StatsData

---
+++ src/math/stats_data.rs
@@ -0,0 +1,71 @@
+use crate::math::array::compare_len;
+use crate::math::stats::WeightMode;
+use crate::math::stats_array::StatsArray;
+use std::iter::Zip;
+
+#[derive(Clone)]
+pub struct StatsData {
+    pub x: StatsArray,
+    pub y: StatsArray,
+}
+
+impl StatsData {
+    pub fn new(x: Vec<f64>, y: Vec<f64>) -> Self {
+        assert_eq!(x.len(), y.len());
+        Self {
+            x: StatsArray::new(x),
+            y: StatsArray::new(y),
+        }
+    }
+
+    pub fn new_statistical_weights(x: Vec<f64>, y: Vec<f64>) -> Self {
+        assert_eq!(x.len(), y.len());
+        let weights: Vec<f64> = y.iter().map(|_y: &f64| _y.powi(-1)).collect();
+        let mode: WeightMode = WeightMode::Statistical;
+        Self {
+            x: StatsArray::new_weighted(x, weights.clone(), mode),
+            y: StatsArray::new_weighted(y, weights, mode),
+        }
+    }
+
+    pub fn new_weighted(x: Vec<f64>, y: Vec<f64>, weights: Vec<f64>, mode: WeightMode) -> Self {
+        assert_eq!(x.len(), y.len());
+        let status: Result<(), String> = compare_len(&x, &weights);
+        match status {
+            Err(_) => Self::new_statistical_weights(x, y),
+            Ok(_) => Self {
+                x: StatsArray::new_weighted(x, weights.clone(), mode),
+                y: StatsArray::new_weighted(y, weights, mode),
+            },
+        }
+    }
+
+    pub fn zipxy(&self) -> Zip<std::slice::Iter<'_, f64>, std::slice::Iter<'_, f64>> {
+        self.x.iter().zip(self.y.iter())
+    }
+
+    pub fn wzipxy(
+        &self,
+    ) -> Zip<Zip<std::slice::Iter<'_, f64>, std::slice::Iter<'_, f64>>, std::slice::Iter<'_, f64>>
+    {
+        self.zipxy().zip(self.x.weights.iter())
+    }
+
+    pub fn sum_of_products(&self) -> f64 {
+        let order: i32 = match self.x.mode {
+            WeightMode::None => return self.x.iter().zip(self.y.iter()).map(|(x, y)| x * y).sum(),
+            WeightMode::Instrumental => -2,
+            WeightMode::Statistical => -1,
+        };
+        self.wzipxy().map(|((x, y), w)| x * y * w.powi(order)).sum()
+    }
+
+    pub fn push(&mut self, x: f64, y: f64, weight: f64) {
+        self.x.push(x, weight);
+        self.y.push(y, weight);
+    }
+
+    pub fn len(&self) -> usize {
+        self.x.len()
+    }
+}
