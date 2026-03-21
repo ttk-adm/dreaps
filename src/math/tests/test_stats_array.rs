40c5cd20aacab018966ade60c3aded52b300d962
ttk-adm <218129711+ttk-adm@users.noreply.github.com>
2026-03-21 23:07:24 +0100

    add tests

---
+++ src/math/tests/test_stats_array.rs
@@ -0,0 +1,81 @@
+#[cfg(test)]
+mod tests {
+    // use super::*;
+    use crate::math::stats::WeightMode;
+    use crate::math::stats_array::StatsArray;
+
+    #[test]
+    fn test_new() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let stat_array: StatsArray = StatsArray::new(array);
+        assert_eq!(stat_array.array, vec![1., 2., 3.]);
+        assert_eq!(stat_array.weights, vec![1., 1., 1.]);
+        assert_eq!(stat_array.mode, WeightMode::None);
+    }
+
+    #[test]
+    fn test_new_weighted() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let weights: Vec<f64> = vec![0.1, 0.2, 0.3];
+        let stat_array: StatsArray =
+            StatsArray::new_weighted(array, weights, WeightMode::Instrumental);
+        assert_eq!(stat_array.array, vec![1., 2., 3.]);
+        assert_eq!(stat_array.weights, vec![0.1, 0.2, 0.3]);
+        assert_eq!(stat_array.mode, WeightMode::Instrumental);
+    }
+
+    #[test]
+    fn test_new_weighted_mismatch() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let weights: Vec<f64> = vec![0.1, 0.2];
+        let stat_array: StatsArray =
+            StatsArray::new_weighted(array, weights, WeightMode::Instrumental);
+        assert_eq!(stat_array.array, vec![1., 2., 3.]);
+        assert_eq!(stat_array.weights, vec![1., 1., 1.]);
+        assert_eq!(stat_array.mode, WeightMode::None);
+    }
+
+    #[test]
+    fn test_sum() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let stat_array: StatsArray = StatsArray::new(array);
+        assert_eq!(stat_array.sum(), 6.);
+        let weights: Vec<f64> = vec![0.1, 0.2, 0.3];
+        let w_stat_array: StatsArray =
+            StatsArray::new_weighted(stat_array.array, weights, WeightMode::Instrumental);
+        assert_eq!(w_stat_array.sum(), 183.33333333333331);
+    }
+
+    #[test]
+    fn test_mean() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let stat_array: StatsArray = StatsArray::new(array);
+        assert_eq!(stat_array.mean(), 2.);
+        let weights: Vec<f64> = vec![0.1, 0.2, 0.3];
+        let w_stat_array: StatsArray =
+            StatsArray::new_weighted(stat_array.array, weights, WeightMode::Instrumental);
+        assert_eq!(w_stat_array.mean(), 1.3469387755102042);
+    }
+
+    #[test]
+    fn test_stdev() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let stat_array: StatsArray = StatsArray::new(array);
+        assert_eq!(stat_array.stdev(), 1.);
+        let weights: Vec<f64> = vec![0.1, 0.2, 0.3];
+        let w_stat_array: StatsArray =
+            StatsArray::new_weighted(stat_array.array, weights, WeightMode::Instrumental);
+        assert_eq!(w_stat_array.stdev(), 1.2805207707796726);
+    }
+
+    #[test]
+    fn test_sum_of_squares() {
+        let array: Vec<f64> = vec![1., 2., 3.];
+        let stat_array: StatsArray = StatsArray::new(array);
+        assert_eq!(stat_array.sum_of_squares(), 14.);
+        let weights: Vec<f64> = vec![0.1, 0.2, 0.3];
+        let w_stat_array: StatsArray =
+            StatsArray::new_weighted(stat_array.array, weights, WeightMode::Instrumental);
+        assert_eq!(w_stat_array.sum_of_squares(), 300.);
+    }
+}
