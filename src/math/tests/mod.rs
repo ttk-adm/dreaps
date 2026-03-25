833067bb83c9ae261391f24a06a9c728f846ba13
ttk-adm <218129711+ttk-adm@users.noreply.github.com>
2026-03-25 22:26:15 +0100

    5 add discrete tests (#9)

--- src/math/tests/mod.rs
+++ src/math/tests/mod.rs
@@ -1,3 +1,4 @@
-mod test_stats_array;
-mod test_stats;
 mod test_array;
+mod test_discrete;
+mod test_stats;
+mod test_stats_array;
--- src/math/tests/test_array.rs
+++ src/math/tests/test_array.rs
@@ -8,6 +8,9 @@ mod test {
         let array_b = [4., 5., 6.];
         let array_c = [1., 2., 3., 4.];
         assert_eq!(compare_len(&array_a, &array_b), Ok(()));
-        assert_eq!(compare_len(&array_a, &array_c), Err("arrays must have equal lengths".into()));
+        assert_eq!(
+            compare_len(&array_a, &array_c),
+            Err("arrays must have equal lengths".into())
+        );
     }
 }
